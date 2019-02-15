d1735d4a534cac7bb956564dd0460214c0fcb054
group~1 <chenxin@chensanjin.xin>
2019-02-15 16:20:07 +0800

    第一次提交

---
+++ src/main/java/RabbitConsumer.java
@@ -0,0 +1,52 @@
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * rabbitmq消费者
+ *
+ * @author chenxinnunu@gmail.com
+ * @date 2019/2/15 16:00
+ */
+public class RabbitConsumer {
+    private static final String QUEUE_NAME = "queue_demo";
+    private static final String IP_ADDRESS = "119.29.222.73";
+    private static final int PORT = 5672;
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Address[] addresses = new Address[]{
+                new Address(IP_ADDRESS, PORT)
+        };
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("123456");
+        //创建连接；这里的连接方式和生产者的demo略有不同，注意区别
+        Connection connection = factory.newConnection(addresses);
+        //创建信道
+        final Channel channel = connection.createChannel();
+        //设置客户端最多接收未被ack（确认）的消息个数
+        channel.basicQos(64);
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                System.out.println("recv message: " + new String(body));
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, consumer);
+        //等待回调函数执行完毕之后，关闭资源
+        TimeUnit.SECONDS.sleep(5);
+        channel.close();
+        connection.close();
+    }
+}
--- src/main/java/RabbitProducer.java
+++ src/main/java/RabbitProducer.java
@@ -1,6 +1,7 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -30,6 +31,16 @@ public class RabbitProducer {
         //创建一个type="direct"、持久化的、非自动删除的交换器
         channel.exchangeDeclare(EXCHANGE_NAME, "direct", true, false, null);
         //创建一个持久化、非排他的、非自动删除的队列
-        channel.queueDeclare();
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        //将交换器与队列通过路由键绑定
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+        //发送一条持久化的消息
+        String message = "Hello World!";
+        channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes());
+        //关闭资源
+        channel.close();
+        connection.close();
     }
 }
